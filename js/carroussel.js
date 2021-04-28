10f321ebb2b1d0175811c48e2468a6ac21047da8
lauracarlotta <carlotta.custodio@gmail.com>
2021-04-28 01:30:51 -0300

    created directory img / inserting image countries .jpg

---
+++ js/carroussel.js
@@ -0,0 +1,69 @@
+let images = [
+
+    UnitedStates = {
+        name: "Nova York",
+        image: "https://images.pexels.com/photos/1770775/pexels-photo-1770775.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+    },
+
+    Canada = {
+        name: "Toronto",
+        image: "https://images.pexels.com/photos/935474/toronto-beauty-clouds-skyline-935474.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+    },
+
+    UnitedKingdon = {
+        name: "Londres",
+        image: "https://images.pexels.com/photos/460672/pexels-photo-460672.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+    }, 
+
+    Spain = {
+        name: "Barcelona", 
+        image: "https://images.pexels.com/photos/4969900/pexels-photo-4969900.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+    },
+
+    France = {
+        name: "Paris",
+        image: "https://images.pexels.com/photos/739407/pexels-photo-739407.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+    },
+
+    Italy = {
+        name: "Roma",
+        image: "https://images.pexels.com/photos/10922/pexels-photo-10922.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+    },
+
+    Greece = {
+        name: "Atenas",
+        image: "https://images.pexels.com/photos/4388164/pexels-photo-4388164.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+    }, 
+
+    Australia = {
+        name: "Sydney",
+        image: "https://images.pexels.com/photos/2193300/pexels-photo-2193300.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+    },
+
+    Japan = {
+        name: "Tokyo",
+        image: "https://images.pexels.com/photos/161164/senso-ji-kyoto-japan-temple-161164.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+    }, 
+
+    Thailand = {
+        name: "Bagkok",
+        image: "https://images.pexels.com/photos/2797526/pexels-photo-2797526.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+    },
+
+    UnitedArabEmirates = {
+        name: "Dubai", 
+        image: "https://images.pexels.com/photos/823696/pexels-photo-823696.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+    }, 
+
+    Chile = {
+        name: "Ilha de Páscoa",
+        image: "https://images.pexels.com/photos/2819082/pexels-photo-2819082.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940mach"
+    },
+
+
+    Peru = {
+        name: "Machu Picchu",
+        image: "https://images.pexels.com/photos/1570610/pexels-photo-1570610.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+    }
+
+];
