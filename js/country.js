ffe4c9ea560a98279b60f478055e6d658b4c9806
William Jonathan <willyeager01@gmail.com>
2021-04-30 18:27:35 -0300

    grid v3

--- js/country.js
+++ js/country.js
@@ -31,38 +31,3 @@ function randomImage() {
 // Tempo em milisegundos
 setInterval(function() { randomImage() }, 10000)
 
-html {
-    font-size: 100%;
-}
-
-body {
-    background-color: blue;
-    font-family: Arial, Helvetica, sans-serif;
-}
-
-.bs-grid-big {
-    background: #fff;
-    margin-bottom: 10px;
-}
-    
-.bs-grid-small {
-    background: #fff;
-    margin-bottom: 10px;
-}
-
-h2 {
-    font-size: 0.875rem;
-    color: red;
-    text-transform: uppercase;
-    font-weight: 600;
-}
-
-p {
-    font-size: 0.75rem;
-}
-      
-@media (min-width: 768px) { html { font-size:100%;} }
-
-@media (min-width: 992px) { html { font-size:120%;} }
-
-@media (min-width: 1200px) { html { font-size:140%; } }
