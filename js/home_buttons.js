a66613d73dca818baf70837e694a8fb4005a1b0e
lauracarlotta <carlotta.custodio@gmail.com>
2021-04-30 23:26:41 -0300

    testing buttons home.html, testing tags object  home

---
+++ js/home_buttons.js
@@ -0,0 +1,10 @@
+function clickButtonProfile(classList) {
+    adventureprofile.classList.remove(".classList");
+    return classList;
+}
+
+let buttonAdvetureProfile = document.querySelector(".button-type-trip-adventure");
+
+let adventureprofile = document.querySelector(".adventure-profile-off")
+
+buttonAdvetureProfile.onclick = clickButtonProfile(".adventure-profile-off");
