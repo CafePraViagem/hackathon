047923e5a0ba6ea7de3e811f075eb6058113e14d
lauracarlotta <carlotta.custodio@gmail.com>
2021-05-02 03:32:08 -0300

    function button active when user click and show div hided (tried construct global function)

--- js/home_buttons.js
+++ js/home_buttons.js
@@ -1,14 +1,22 @@
-function clickButtonTypeTripAdventure() {
+function clickButtonTypeTrip(profileOn, profileOff) {
 
-    let divAdventureProfileOn = document.querySelector(".adventure-profile-on");
-    divAdventureProfileOn.classList.remove("adventure-profile-off");
+    let divProfileOn = document.getElementsByClassName(`${profileOn}`);
+    divProfileOn.classList.remove(profileOff);
 
     console.log(buttonTypeTripAdventure.value);
+    
+    /* let divProfileOn = `<div class="${profileOn}>`;
+    let divProfileOff = `<div class="${profileOff}">`
+    divProfileOn.classList.remove(divProfileOff);
+
+    console.log(buttonTypeTripAdventure.value); */
 }
 
 let buttonTypeTripAdventure = document.querySelector(".button-type-trip-adventure");
 
-buttonTypeTripAdventure.onclick = clickButtonTypeTripAdventure;
+buttonTypeTripAdventure.onclick = clickButtonTypeTrip("adventure-profile-on",".adventure-profile-off");
+
+
 
 /* function clickButtonTypeTripAdventure() {
 
