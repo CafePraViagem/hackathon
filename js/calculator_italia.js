26c9ebe94f29f18efc4e83344cb74d212936a702
lauracarlotta <carlotta.custodio@gmail.com>
2021-05-05 22:25:14 -0300

    corected commit

--- js/calculator_italia.js
+++ js/calculator_italia.js
@@ -265,7 +265,7 @@ function somaExtra(){
 function calculaExtra(){
     let precoExtra = somaExtra();
     let output = document.getElementById("output6");
-    output.innerHTML = `Total: R$ ${precoExtra}`;
+    output.innerHTML = `Toral: R$ ${precoExtra}`;
 }
 
 function getNumeroViajantes(){
