b38857386fdc0491e4ed0683b8480826b7c7b9b0
lauracarlotta <carlotta.custodio@gmail.com>
2021-05-09 15:02:52 -0300

    form validations test / carousel tests

--- js/carousel.js
+++ js/carousel.js
@@ -1,56 +1,51 @@
-/* let imagesBanners = [
+let imagesBanner = document.querySelectorAll("#carousel .carousel-item");
+let maxImagesBanner = imagesBanner.length;
+let imagesAdventurous = document.querySelectorAll("#adventurous .carousel-item");
+let maxImagesAdventurous = imagesAdventurous.length;
+let imagesYoung = document.querySelectorAll("#young .carousel-item");
+let maxImagesYoung = imagesYoung.length;
+let time = 3000;
+let i = 0;
 
-    UnitedStates = {
-        name: "Nova York",
-        image: "https://images.pexels.com/photos/1770775/pexels-photo-1770775.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-        alt: "Cidade de Nova York"
-    },
 
-    Spain = {
-        name: "Barcelona", 
-        image: "https://images.pexels.com/photos/1388030/pexels-photo-1388030.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-        alt: "Cidade de Barcelona"
-    },
+function nextImage(images, maxImages) {
 
-    France = {
-        name: "Paris",
-        image: "https://images.pexels.com/photos/739407/pexels-photo-739407.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-        alt: "Cidade de Paris"
-    },
+    images[i].classList.remove("active");
 
-    Italy = {
-        name: "Roma",
-        image: "https://images.pexels.com/photos/1797161/pexels-photo-1797161.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-        alt: "Cidade de Roma"
-    }, 
+    i++
 
-    Germany = {
-        name: "Berlim", 
-        image: "https://images.pexels.com/photos/1963082/pexels-photo-1963082.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-        alt: "Cidade de Berlim"
-    },
-
-    Peru = {
-        name: "Machu Picchu",
-        image: "https://images.pexels.com/photos/1570610/pexels-photo-1570610.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-        alt: "Cidade de Machu Picchu"
+    if (i >= maxImages ) {
+        i = 0;
     }
 
-];
-
-function carouselItem () {
-    let divCarouselItem = document.querySelector(".carousel-item");
-
-    for(let i = 0; i < imagesBanners.length; i++) {
-        divCarouselItem.innerHTML = `<div class="carousel-item"><img class="image-width-height-carousel" src="${imagesBanners.image[i]}" alt="${imagesBanners.alt[i]}"> </div> `;
-    }
+    images[i].classList.add("active");
 }
 
-setInterval(carouselItem, 2000); */
+function startImagesMainBanner () {
+    
+    setInterval(() => {
+        nextImage(imagesBanner, maxImagesBanner);
+    }, time);
+}
 
+function startImagesTypeAdventurous () {
+    
+    setInterval(() => {
+        nextImage(imagesAdventurous, maxImagesAdventurous);
+    }, time);
+}
 
-$('.carousel').carousel()
+function startImagesTypeYoung () {
+    
+    setInterval(() => {
+        nextImage(imagesYoung, maxImagesYoung);
+    }, time);
+}
 
-$('.carousel').carousel({
-    interval: 1000
-})
+window.addEventListener("load", startImagesMainBanner);
+
+let buttonTypeTripAdventurous = document.querySelector(".button-type-trip-adventure");
+buttonTypeTripAdventurous.addEventListener("click", startImagesTypeAdventurous);
+
+let buttonTypeTripYoung = document.querySelector(".button-type-trip-young");
+buttonTypeTripYoung.addEventListener("click", startImagesTypeYoung);
