d0c6e664fad8ede4c06bbf85f384b92494a2f7cb
lauracarlotta <carlotta.custodio@gmail.com>
2021-05-09 20:57:38 -0300

    renamed conheça seu destino to perfil de viagem

--- test.js
+++
@@ -1,9 +0,0 @@
-var userEmail = "laura@laura.com";
-
-console.log(userEmail.indexOf("@"))
-console.log(userEmail.indexOf(".com"))
-
-if (userEmail.indexOf("@") == -1 || userEmail.indexOf(".com") == -1) {
-   console.log("entrou")
-} 
-
