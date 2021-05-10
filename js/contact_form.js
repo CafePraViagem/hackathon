dc99e3316769774bd08fd5cbfe4c83c82495d6b5
Adelar Junior <adelarjr.s@gmail.com>
2021-05-10 17:15:53 -0300

    fix/html fixes for phone validation

--- js/contact_form.js
+++ js/contact_form.js
@@ -1,23 +1,42 @@
 let userName = formUserContact.name.value;
 let userBirthdate = formUserContact.nasc.value;
 let userCity = formUserContact.city.valeu;
-let userEmail  = formUserContact.email.value;
+let userEmail = formUserContact.email.value;
 let userPhone = formUserContact.phone.value;
 let userMessage = formUserContact.message.value;
 
-function formValidation() {
-  
-  if (userMessage.length > 1 && userMessage.length <= 2000) {
-    if (userMessage > 2000) {
-      formUserContact.message.focus();
-      clickButtonOutputChangeClass(outputMessage, ".output-message-off", "output-message-on");
-      ;
-    }
-  } 
+document.getElementById('userPhone').addEventListener('keydown', function () {
+  const phone = document.getElementById('userPhone');
+  const re = /^\(?\d{2}\)?[-. ]?\d{5}[-. ]?\d{4}$/;
 
+  if (!re.test(phone.value)) {
+    phone.classList.add('is-invalid')
+  } else {
+    phone.classList.remove('is-invalid')
+  }
+});
+
+function formValidation() {
+
+  if (document.getElementById('userPhone').classList.contains('is-invalid')) {
+    // alert('Erro! Preencha todos os campos corretamente.');
+    formUserContact.phone.focus();
+  } else {
+    if (userMessage.length > 1 && userMessage.length <= 2000) {
+      if (userMessage > 2000) {
+        formUserContact.message.focus();
+        clickButtonOutputChangeClass(outputMessage, ".output-message-off", "output-message-on");
+      }
+    }
+
+    document.getElementById('formContact').addEventListener('submit', function (event) {
+      event.preventDefault()
+      startModal("modalResult")
+    })
+  }
 }
 
-function clickButtonOutputChangeClass(tagId, outputOff, outputOn){
+function clickButtonOutputChangeClass(tagId, outputOff, outputOn) {
 
   tagId.classList.remove(outputOff);
   tagId.classList.add(outputOn);
@@ -29,13 +48,14 @@ function clickButtonOutputChangeClass(tagId, outputOff, outputOn){
 }
 
 let userNameId = document.querySelector("#userName");
-userNameId.addEventListener("keypress", function(e){
+userNameId.addEventListener("keypress", function (e) {
 
   if (!checkLetterInputName(e)) {
     e.preventDefault();
   }
-  
+
 });
+
 function checkLetterInputName(e) {
 
   const charLetter = String.fromCharCode(e.keyCode);
@@ -57,12 +77,12 @@ function checkLetterInputName(e) {
 }
 
 let userCityId = document.querySelector("#userCity");
-userCityId.addEventListener("keypress", function(e){
+userCityId.addEventListener("keypress", function (e) {
 
   if (!checkLetterInputCity(e)) {
     e.preventDefault();
   }
-  
+
 });
 function checkLetterInputCity(e) {
 
@@ -78,7 +98,7 @@ function checkLetterInputCity(e) {
     return true;
 
   } else {
-    
+
     formUserContact.city.focus();
     clickButtonOutputChangeClass(outputCity, "output-city-off", "output-city-on");
   }
@@ -88,7 +108,7 @@ var checkboxNotBrazil = document.querySelector("#doNotLiveInBrazil");
 checkboxNotBrazil.onchange = function () {
 
   if (checkboxNotBrazil.checked) {
-    
+
     var userCity = document.querySelector("#userCity");
     var userState = document.querySelector("#userState");
     var userPhone = document.querySelector("#userPhone");
@@ -120,12 +140,12 @@ checkboxNotBrazil.onchange = function () {
 };
 
 //  ----> SHOW MODAL
- function startModal(modalID) {
+function startModal(modalID) {
   const modalBox = document.getElementById(modalID);
   var nome = $("#nome").val();
-        $("#conteudo").empty();
-        $("#conteudo").append("Nome: " + nome);
-   
+  $("#conteudo").empty();
+  $("#conteudo").append("Nome: " + nome);
+
   modalBox.classList.add("showModal");
   modalBox.addEventListener("click", (event) => {
     if (event.target.id == modalID || event.target.id == "close") {
@@ -134,11 +154,3 @@ checkboxNotBrazil.onchange = function () {
     }
   });
 }
-
-document.getElementById('formContact').addEventListener('submit', function(event) {
-  event.preventDefault()
-  startModal("modalResult")
-})
-
-/* let buttonSubmit = document.getElementById("enviar");
-buttonSubmit.onclick = formValidation(); */
